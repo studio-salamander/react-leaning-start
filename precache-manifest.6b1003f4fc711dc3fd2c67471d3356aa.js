2abb642bc52df8d8b8aa1c352ef513330e44ee9c
Andy Radkevich <andy@yoola.com>
2020-05-05 16:22:57 +0300

    Updates

---
+++ precache-manifest.6b1003f4fc711dc3fd2c67471d3356aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d321925caa26c578051ed24af935cc",
+    "url": "/react-leaning-start/index.html"
+  },
+  {
+    "revision": "fe738d9782980b25f162",
+    "url": "/react-leaning-start/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "bd112cbd96f5c4725dbd",
+    "url": "/react-leaning-start/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "fe738d9782980b25f162",
+    "url": "/react-leaning-start/static/js/2.28a7f3c8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-leaning-start/static/js/2.28a7f3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd112cbd96f5c4725dbd",
+    "url": "/react-leaning-start/static/js/main.395b0f40.chunk.js"
+  },
+  {
+    "revision": "3bb81612d3cecfc848dc",
+    "url": "/react-leaning-start/static/js/runtime-main.6b737b52.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-leaning-start/precache-manifest.6b1003f4fc711dc3fd2c67471d3356aa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-leaning-start/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
